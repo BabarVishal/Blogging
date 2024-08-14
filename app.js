8765f43dade8c4cb261105c4f77605dc35e1d38c
vishal <vishalbabar7351@gmail.com>
2024-08-14 23:45:53 +0530

    updeted my code

--- app.js
+++ app.js
@@ -1,40 +1,44 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-const Userrouter = require("./routes/user.router")
-const BlogRoute = require("./routes/blog.routes")
-const mongoose = require("mongoose");
-const cookiepaser = require("cookie-parser");
-const { checkForauthenticationCookie } = require("./middleware/authentication");
-const Blog = require("./models/blog.model");
-
 require("dotenv").config();
-const PORT = process.env.PORT || 8000
 
-//DbConection
-mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log("MongoDb Conected!"))
-.catch((err) => console.log("mongoDb Disconected!", err))
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
-//middleware
-app.use(express.urlencoded({extended: false}));
-app.use(cookiepaser());
-app.use(checkForauthenticationCookie("token"));
-app.use(express.static(path.resolve("./public")))
+const Blog = require("./models/blog");
 
-//Frentend.....
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
+
+const app = express();
+const PORT = process.env.PORT || 8000; // Changed from 6000 to 8000
+
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected"));
+
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-app.get("/", async (req, res)=>{
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", async (req, res) => {
   const allBlogs = await Blog.find({});
-  return res.render("home",{
+  res.render("home", {
     user: req.user,
     blogs: allBlogs,
   });
-})
+});
 
-app.use("/user", Userrouter);
-app.use("/Blog", BlogRoute);
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
 
-app.listen(PORT, ()=> console.log(`Server Stated In PORT: ${PORT}`));
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
